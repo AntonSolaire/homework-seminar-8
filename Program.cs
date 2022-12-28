6fa7feeadde6b16232f21ed4a0ac36544c39abb6
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-28 20:50:21 +0300

    Не получилось осилить четвертую

--- Program.cs
+++ Program.cs
@@ -81,7 +81,6 @@
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 
-
 // int[,] Create2DRandomArray(int rows, int colomns, int minValue, int maxValue)
 // {
 //     int[,] newArray = new int[rows, colomns];
@@ -163,73 +162,81 @@
 // 15 18
 
 
+// int[,] Create2DRandomArray(int rows, int colomns, int minValue, int maxValue)
+// {
+//     int[,] newArray = new int[rows, colomns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colomns; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void Multiplication(int[,] firstMatrix, int[,] secondMatrix, int[,] finalMatrix)
+// {
+//     for (int i = 0; i < finalMatrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < finalMatrix.GetLength(1); j++)
+//         {
+//             int result = 0;
+//             for (int k = 0; k < firstMatrix.GetLength(1); k++)
+//             {
+//                 result += firstMatrix[i,k] * secondMatrix[k,j];
+//             }
+//             finalMatrix[i,j] = result;
+//         }
+//     }
+// }
+
+// Console.WriteLine($"Для умножения количество столбцов первой должно быть равно количеству строк второй");
+
+// Console.WriteLine("Введите количество строк 1 матрицы");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов 1 матрицы и строк 2-й:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов 2 матрицы:");
+// int p = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное возможное значение элемента массива:");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
 
 
-int[,] Create2DRandomArray(int rows, int colomns, int minValue, int maxValue)
-{
-    int[,] newArray = new int[rows, colomns];
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < colomns; j++)
-        {
-            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
-        }
-    }
-    return newArray;
-}
+// int[,] firstMatrix = Create2DRandomArray(m, n, min, max);
+// Console.WriteLine("Первая матрица:");
+// Show2DArray(firstMatrix);
 
-void Show2DArray(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
-    }
-}
+// int[,] secondMatrix = Create2DRandomArray(n, p, min, max);
+// Console.WriteLine("Вторая матрица:");
+// Show2DArray(secondMatrix);
 
-void Multiplication(int[,] firstMatrix, int[,] secondMatrix, int[,] finalMatrix)
-{
-    for (int i = 0; i < finalMatrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < finalMatrix.GetLength(1); j++)
-        {
-            int result = 0;
-            for (int k = 0; k < firstMatrix.GetLength(1); k++)
-            {
-                result += firstMatrix[i,k] * secondMatrix[k,j];
-            }
-            finalMatrix[i,j] = result;
-        }
-    }
-}
+// int[,] finalMatrix = new int[m, p];
 
-Console.WriteLine($"Для умножения количество столбцов первой должно быть равно количеству строк второй");
-
-Console.WriteLine("Введите количество строк 1 матрицы");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов 1 матрицы и строк 2-й:");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов 2 матрицы:");
-int p = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите минимальное возможное значение элемента массива:");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите максимально возможное значение элемента массива:");
-int max = Convert.ToInt32(Console.ReadLine());
+// Multiplication(firstMatrix, secondMatrix, finalMatrix);
+// Console.WriteLine($"Произведение матриц:");
+// Show2DArray(finalMatrix);
 
 
-int[,] firstMatrix = Create2DRandomArray(m, n, min, max);
-Console.WriteLine("Первая матрица:");
-Show2DArray(firstMatrix);
 
-int[,] secondMatrix = Create2DRandomArray(n, p, min, max);
-Console.WriteLine("Вторая матрица:");
-Show2DArray(secondMatrix);
 
-int[,] finalMatrix = new int[m, p];
-
-Multiplication(firstMatrix, secondMatrix, finalMatrix);
-Console.WriteLine($"Произведение матриц:");
-Show2DArray(finalMatrix);
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
