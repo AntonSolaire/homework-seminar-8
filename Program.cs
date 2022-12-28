79584189d4d552ac9dfbd4ca3bd9fee04a518881
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-28 20:44:19 +0300

    Сделал третью

--- Program.cs
+++ Program.cs
@@ -82,6 +82,89 @@
 
 
 
+// int[,] Create2DRandomArray(int rows, int colomns, int minValue, int maxValue)
+// {
+//     int[,] newArray = new int[rows, colomns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colomns; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// int Sum(int[,] array)
+// {
+//     int min = 0;
+//     int sumElements = 0;
+//     for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 sumElements += array[0, j];
+//                 min = sumElements;
+//             }
+
+//     if (array.GetLength(0) != array.GetLength(1))
+//         Console.WriteLine("Массив не прямоугольный");
+//     else
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 sumElements += array[i, j];
+//             }
+
+//             if (sumElements<min){
+//             min = sumElements;
+//             sumElements = 0;
+//             }
+//             else sumElements = 0;
+//         }
+//     return min;
+// }
+
+
+// Console.WriteLine("Введите количество строк");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное возможное значение элемента массива:");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = Create2DRandomArray(m, n, min, max);
+// Show2DArray(myArray);
+// Console.WriteLine();
+// int result = Sum(myArray);
+// Console.WriteLine(result);
+
+
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+
+
 int[,] Create2DRandomArray(int rows, int colomns, int minValue, int maxValue)
 {
     int[,] newArray = new int[rows, colomns];
@@ -107,47 +190,46 @@ void Show2DArray(int[,] array)
     }
 }
 
-int Sum(int[,] array)
+void Multiplication(int[,] firstMatrix, int[,] secondMatrix, int[,] finalMatrix)
 {
-    int min = 0;
-    int sumElements = 0;
-    for (int j = 0; j < array.GetLength(1); j++)
-            {
-                sumElements += array[0, j];
-                min = sumElements;
-            }
-
-    if (array.GetLength(0) != array.GetLength(1))
-        Console.WriteLine("Массив не прямоугольный");
-    else
-        for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < finalMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < finalMatrix.GetLength(1); j++)
         {
-            for (int j = 0; j < array.GetLength(1); j++)
+            int result = 0;
+            for (int k = 0; k < firstMatrix.GetLength(1); k++)
             {
-                sumElements += array[i, j];
+                result += firstMatrix[i,k] * secondMatrix[k,j];
             }
-            
-            if (sumElements<min){
-            min = sumElements;
-            sumElements = 0;
-            }
-            else sumElements = 0;
+            finalMatrix[i,j] = result;
         }
-    return min;
+    }
 }
 
+Console.WriteLine($"Для умножения количество столбцов первой должно быть равно количеству строк второй");
 
-Console.WriteLine("Введите количество строк");
+Console.WriteLine("Введите количество строк 1 матрицы");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов:");
+Console.WriteLine("Введите количество столбцов 1 матрицы и строк 2-й:");
 int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов 2 матрицы:");
+int p = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите минимальное возможное значение элемента массива:");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимально возможное значение элемента массива:");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = Create2DRandomArray(m, n, min, max);
-Show2DArray(myArray);
-Console.WriteLine();
-int result = Sum(myArray);
-Console.WriteLine(result);
+
+int[,] firstMatrix = Create2DRandomArray(m, n, min, max);
+Console.WriteLine("Первая матрица:");
+Show2DArray(firstMatrix);
+
+int[,] secondMatrix = Create2DRandomArray(n, p, min, max);
+Console.WriteLine("Вторая матрица:");
+Show2DArray(secondMatrix);
+
+int[,] finalMatrix = new int[m, p];
+
+Multiplication(firstMatrix, secondMatrix, finalMatrix);
+Console.WriteLine($"Произведение матриц:");
+Show2DArray(finalMatrix);
